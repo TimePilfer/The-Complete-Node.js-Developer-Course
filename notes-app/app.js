df99a068c822691529183804c13cbb09e0bba840
James Pierson <james.scott.pierson@gmail.com>
2021-03-30 15:25:26 -0400

    3.8 Final Update

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is James')
+//fs.writeFileSync('notes.txt', 'My name is James')
+fs.appendFileSync('notes.txt', "\r\n" + 'This is the appended sentence')
