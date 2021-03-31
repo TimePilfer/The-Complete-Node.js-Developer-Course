78bc0729ff5b9f1a570c15a2efa9f9bdb2de4907
James Pierson <james.scott.pierson@gmail.com>
2021-03-31 14:27:32 -0400

    3.13 Chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,7 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const message = getNotes()
 
 console.log(message)
-
-console.log(validator.isURL('https/mead.io'))
+console.log(chalk.green('Success!'))
