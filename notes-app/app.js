4b3748390843e082145c5babbf5722d2ee8c806a
James Pierson <james.scott.pierson@gmail.com>
2021-04-01 09:24:23 -0400

    4.15 Getting console input from user

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const message = getNotes()
+const command = process.argv[2]
 
-console.log(message)
-console.log(chalk.red('Error!'))
+console.log(process.argv)
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
