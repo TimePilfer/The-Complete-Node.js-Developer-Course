e95371bfcb91ef801301605c0004ac79a22154e3
James Pierson <james.scott.pierson@gmail.com>
2021-04-05 14:14:42 -0400

    4.17 Yargs Parsing pt 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,68 @@
 const chalk = require('chalk')
+const { describe } = require('yargs')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+//Create add command
+yargs.command(
+    {
+        command: 'add',
+        describe: 'Add a new note',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: 'Note body',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: function(argv) {
+            console.log('Title: ', argv.title)
+            console.log('Body: ', argv.body)
+        }
+    }
+)
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+//Create a remove command
+yargs.command(
+    {
+        command: 'remove',
+        describe: 'Remove a note',
+        handler: function() {
+            console.log('Removing the note!')
+        }
+    }
+)
+
+//Create add command
+yargs.command(
+    {
+        command: 'list',
+        describe: 'List all the notes',
+        handler: function() {
+            console.log('Listing all the notes!')
+        }
+    }
+)
+
+//Create a remove command
+yargs.command(
+    {
+        command: 'read',
+        describe: 'Read a note',
+        handler: function() {
+            console.log('Reading the note!')
+        }
+    }
+)
+
+//add, remove, read, list
+
+yargs.parse()
