5ba3de8b9569cf80607851d8f51d372ad6e3d07c
James Pierson <james.scott.pierson@gmail.com>
2021-04-13 13:36:03 -0400

    6.32 Weatherstack api calls and retrieving data

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,11 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=6d30da04ea13e13f993d52804cfd5266&query=Whitby%20On'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + 
+                '. It is currently ' + 
+                response.body.current.temperature + 
+                ' degrees out. It feels like ' + 
+                response.body.current.feelslike + 
+                ' degrees out. ')
 })
