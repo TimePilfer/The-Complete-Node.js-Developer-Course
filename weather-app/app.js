b7a73e62a69ff71d49dd3e1da5347adba0ce5235
James Pierson <james.scott.pierson@gmail.com>
2021-04-15 12:43:46 -0400

    6.36 Geocode with callbacks refactoring

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,27 @@
 const request = require('request')
 
+const geocode = require('./utils/geocode.js')
+
 const url = 'http://api.weatherstack.com/current?access_key=6d30da04ea13e13f993d52804cfd5266&query=Whitby%20On'
 
 request({url: url, json: true}, (error, response) => {
-    console.log(response.body.current.weather_descriptions[0] + 
-                '. It is currently ' + 
-                response.body.current.temperature + 
-                ' degrees out. It feels like ' + 
-                response.body.current.feelslike + 
-                ' degrees out. ')
+
+    if(error) {
+        console.log('Unable to connect to weather service')
+    } else if(response.body.error) {
+        console.log(response.body.error.info)
+    }
+    else {
+        console.log(response.body.current.weather_descriptions[0] + 
+                    '. It is currently ' + 
+                    response.body.current.temperature + 
+                    ' degrees out. It feels like ' + 
+                    response.body.current.feelslike + 
+                    ' degrees out. ')
+    }
+})
+
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
